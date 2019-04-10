3c16c8f032444e91d457c5d869a1092d96bed9b1
Nikhil Prakash Patil <nikhilpatil.patil0@gmail.com>
2019-04-10 20:47:39 +0530

    hello repeat

---
+++ hellorepeat.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 10 19:58:09 2019
+
+@author: GRENTOR
+"""
+
+a=int(input())
+for i in range (a):
+    if i!=a-1:
+        print("Hello",end='\n')
+    else:
+        print("Hello",end='')
+    print("Hello")
