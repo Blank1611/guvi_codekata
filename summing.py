84ba5479f47c24cef427bd194ea74ee970356b45
Nikhil Prakash Patil <nikhilpatil.patil0@gmail.com>
2019-04-14 11:47:10 +0530

    summning

---
+++ summing.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 14 11:37:06 2019
+
+@author: GRENTOR
+"""
+
+def summing(b):
+    sum=0
+    for i in range(b+1):
+        sum+=i
+    print(sum)
+a = int(input())
+summing(a)
