4fc5851522af84a2c5d22451b6425deba279da8e
Nikhil Prakash Patil <nikhilpatil.patil0@gmail.com>
2019-06-19 14:47:12 +0530

    index same

---
+++ untitled5.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 19 14:42:18 2019
+
+@author: GRENTOR
+"""
+
+a = int(input())
+b = list(map(int, input().split()))
+c=[]
+for i in range(a):
+    if(i == b[i]):
+        c.append(b[i])
+c.sort()
+print(*c)
