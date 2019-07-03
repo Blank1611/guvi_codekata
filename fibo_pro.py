077c98529f1a3ba3071975aff9b0768aff9697d2
Nikhil Prakash Patil <nikhilpatil.patil0@gmail.com>
2019-07-04 00:27:39 +0530

    some changes

--- fibo_pro.py
+++ fibo_pro.py
@@ -4,9 +4,9 @@ Created on Thu Jul  4 00:00:11 2019
 
 @author: GRENTOR
 """
-def fibo(n):
+def fibo(z):
     s=0
-    for i in range(0,n):
+    for i in range(0,z):
         s+=i
     return s
 a = int(input())
