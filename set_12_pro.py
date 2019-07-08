1cbc3dc4c4169a4cc2c5895e7be944a0b1439813
Nikhil Prakash Patil <nikhilpatil.patil0@gmail.com>
2019-07-08 22:58:47 +0530

    pro 12

---
+++ set_12_pro.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  8 21:34:43 2019
+
+@author: GRENTOR
+"""
+
+N,Q = map(int, input().split())
+liste = list(map(int, input().split()))
+for i in range(Q):
+    a,b = map(int, input().split())
+    sa = lambda a,b: sum(liste[a-1:b+1])
+    print(sa(a,b))
