9ee529dd005beb84fe111b944c24804f8203d152
Nikhil Prakash Patil <nikhilpatil.patil0@gmail.com>
2019-07-09 00:12:55 +0530

    pro 13

--- pro_set14.py
+++ pro_set14.py
@@ -1,14 +1,20 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Jul  8 23:39:07 2019
+Created on Tue Jul  9 00:11:15 2019
 
 @author: GRENTOR
 """
+
+def xor(l):
+    el = 0
+    for i in l:
+        el^=i
+    return el
 n,q = map(int, input().split())
 liste = list(map(int, input().split()))
 for i in range(q):
     a,b = map(int, input().split())
-    sa = lambda a,b: min(liste[a-1:b])
-    print(sa(a,b))
+    sa = xor(liste[a-1:b])
+    print(sa)
     
-    
+#[el^=elem for elem in l[a-1:b]]
