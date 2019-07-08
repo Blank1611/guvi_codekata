0e08218e38b53e265160a8762e52503bdc7ea60b
Nikhil Prakash Patil <nikhilpatil.patil0@gmail.com>
2019-07-08 20:57:43 +0530

    ki

--- fibo13_pro.py
+++ fibo13_pro.py
@@ -4,12 +4,13 @@ Created on Thu Jul  4 00:00:11 2019
 
 @author: GRENTOR
 """
-def fibo(z):
-    s=0
-    for i in range(0,z):
-        s+=i
-    return s
+
+
 a = int(input())
 b = list(map(int, input().split()))
-result = list(map(fibo, b))
-print(sum(result), end = "")
+summ = 0
+for i in range(1,len(b)):
+    for x in b[:i]:
+        if x<b[i]:
+            summ+=x
+print(summ, end = "")
