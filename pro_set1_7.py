d95f01220df704471c082bbc6f8f0edb26ce4943
Nikhil Prakash Patil <nikhilpatil.patil0@gmail.com>
2019-08-08 02:37:00 +0530

    set1-7

--- pro_set1_7.py
+++ pro_set1_7.py
@@ -7,5 +7,5 @@ Created on Thu Aug  8 01:27:42 2019
 
 import math
 a = int(input())
-f = map(lambda x: x - 2**(int(math.log2(x))) if x%2!=0 else 0)
+f = lambda x: x - 2**(int(math.log2(x))) if x%2!=0 else 0
 print(f(a))
