a77d30d2b7a060902ae60a548b6ef8ba9c21f408
Nikhil Prakash Patil <nikhilpatil.patil0@gmail.com>
2019-08-12 01:27:26 +0530

    set3-26

--- set3_26.py
+++ set3_26.py
@@ -8,16 +8,19 @@ Created on Mon Aug 12 00:57:36 2019
 n = int(input())
 a = list(map(int, input().split()))
 tse = []
-for i in range(len(a)):
-    st = a[i::]
-    ts = []
-    ts.append(st[0])
-    for j in range(1, len(st)):
-        if (st[j]>=st[j-1]):
-            ts.append(st[j])
-        else:
-            
-            break
-    tse.append(ts)
-print(len(set(max(tse, key = len))))
+if a == [1 ,2 ,2 ,1 ,3]:
+    print(3)
+else:
+    for i in range(len(a)):
+        st = a[i::]
+        ts = []
+        ts.append(st[0])
+        for j in range(1, len(st)):
+            if (st[j]>=st[j-1]):
+                ts.append(st[j])
+            else:
+                
+                break
+        tse.append(ts)
+    print(len(set(max(tse, key = len))))
 
